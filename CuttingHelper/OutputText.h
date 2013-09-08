ef943fea42e7b8cbdfb26cd22a89397b86a5b00c
WhiteSquall <whitesquall@gmail.com>
2013-09-08 15:00:21 +0900

    재정비로 github에 commit

---
+++ CuttingHelper/OutputText.h
@@ -0,0 +1,13 @@
+//
+//  OutputText.h
+//  CuttingHelper
+//
+//  Created by WhiteSquall on 2013. 9. 8..
+//  Copyright (c) 2013년 WhiteSquall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OutputText : NSObject
+
+@end
