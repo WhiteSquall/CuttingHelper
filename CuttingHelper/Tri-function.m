211afa7ace96385a96e7991eaa23bb8a2461cce1
WhiteSquall <k1kmh@hotmail.com>
2015-12-18 20:11:17 +0900

    필요없는거 부터 지우기

--- CuttingHelper/Tri-function.m
+++ CuttingHelper/Tri-function.m
@@ -7,7 +7,45 @@
 //
 
 #import "Tri-function.h"
+#import "math.h"
+
+#define PI 3.14159265
 
 @implementation Tri_function
+
+- (float) updateTri :(float)mAngle :(float)mCLine :(float)mVLine :(float)mHLine
+{
+    int m = 0;
+    
+    float oA = mAngle;
+    float oB = mCLine;
+    float oC = mVLine;
+    float oD = mHLine;
+    
+    if (mAngle != 0) {
+        m = m + 1;
+    }
+    if (mCLine != 0) {
+        m = m + 4;
+    }
+    if (mVLine != 0) {
+        m = m + 6;
+    }
+    if (mHLine != 0) {
+        m = m + 8;
+    }
+    
+    switch (m) {
+        case 5:
+            oC = mCLine * sin(mAngle) * PI / 180;
+            oD = mCLine * cos(mAngle) * PI / 180;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return oA, oB, oC, oD;
+}
 
 @end
